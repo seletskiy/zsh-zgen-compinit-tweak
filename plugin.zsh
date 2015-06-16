32fb397b5921f4dc8293824f69aa5cd786df13f2
Stanislav Seletskiy <s.seletskiy@gmail.com>
2015-06-16 16:47:22 +0600

    screw it! aaaaaaaaaaaaaaaa

--- plugin.zsh
+++ plugin.zsh
@@ -1,2 +1,2 @@
 alias compinit='compinit-zgen'
-function compinit-zgen() { grep -q ".zgen" <<< "${@}" && \compinit -C "${@}" }
+function compinit-zgen() { grep -q ".zgen" <<< "${@}" && \compinit "${@}" }
